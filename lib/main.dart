853dba5a7ff57852f9c89571d559a5702e0dbf19
kona5884 <sho7n.57@gmail.com>
2021-07-13 18:54:51 +0900

    a

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,6 +34,20 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Position position;
+  @override
+  void initState() {
+    super.initState();
+    _getLocation(context);
+  }
+  Future<void> _getLocation(context) async {
+    Position _currentPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high); // ここで精度を「high」に指定している
+    print(_currentPosition);
+    setState(() {
+      position = _currentPosition;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: _image == null
-            ? Text('No image selected.')
+            ? Text('${position}')
             : Image.file(_image),
       ),
       floatingActionButton: FloatingActionButton(
@@ -49,5 +64,40 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.add_a_photo),
       ),
     );
+
+    // return FutureBuilder<GeolocationStatus>(
+    //   future: Geolocator().checkGeolocationPermissionStatus(),
+    //   builder:
+    //       (BuildContext context, AsyncSnapshot<GeolocationStatus> snapshot) {
+    //     if (!snapshot.hasData) {
+    //       return const Center(child: CircularProgressIndicator());
+    //     }
+    //
+    //     if (snapshot.data == GeolocationStatus.denied) {
+    //       return Text(
+    //         'Access to location denied',
+    //         textAlign: TextAlign.center,
+    //       );
+    //     }
+
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Location Infomation",
+                style: TextStyle(
+                  fontSize: 20.0
+                ),
+              ),
+              Text("Your Current Location is :"),
+              Text("${position}")
+            ],
+          ),
+        );
+      // }
+    // );
+
   }
 }
